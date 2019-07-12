1aede446f3ee4966bad04f12aea8a82611583780
tuna1234567890 <48656531+tuna1234567890@users.noreply.github.com>
2019-07-12 12:06:22 +0300

    product
    first

---
+++ js/ilave.js
@@ -0,0 +1,18 @@
+var tarih = new Date();
+var yil = tarih.getFullYear();
+document.querySelector('.copyright').innerText = '\u00A9 '+yil+' '+document.querySelector('.copyright').innerText;
+element = document.querySelectorAll('[href="#tel"]');
+kosul = element.length;
+element[0].innerText = atob('MDUzMiAwNjUgNzggMTY=');
+for(i=0;i<kosul;i++)
+{
+	element[i].setAttribute('href',atob('dGVsOjA1MzIgMDY1IDc4IDE2'));
+}
+
+element = document.querySelectorAll('[href="#mail"]');
+kosul = element.length;
+for(i=0;i<kosul;i++)
+{
+	element[i].setAttribute('href',atob('bWFpbHRvOmluZm9AZGV0YXlkZWRla3RpZmxpay5jb20='));
+	element[i].innerText = atob('aW5mb0BkZXRheWRlZGVrdGlmbGlrLmNvbQ==');
+}
