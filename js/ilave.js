145832d9da52753875f757536b6713139e78dc6a
tuna1234567890 <48656531+tuna1234567890@users.noreply.github.com>
2019-08-08 11:37:43 +0300

    product

--- js/ilave.js
+++ js/ilave.js
@@ -17,6 +17,13 @@ for(i=0;i<kosul;i++)
 	element[i].setAttribute('href',atob('dGVsOjA1MzIgMDY1IDc4IDE2'));
 }
 ///////////////////////////
+element = document.querySelectorAll('[href="#tel3"]');
+kosul = element.length;
+for(i=0;i<kosul;i++)
+{
+	element[i].setAttribute('href',atob('dGVsOjA1MzIgMDY1IDc4IDE2'));
+}
+///////////////////////////
 element = document.querySelectorAll('[href="#mail"]');
 kosul = element.length;
 for(i=0;i<kosul;i++)
